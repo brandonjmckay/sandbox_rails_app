a612482441d9bcaea40e3991ac09990fbfbd791a
stevenallen05 <sallen@amberstyle.ca>
2020-03-13 14:15:00 -0700

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
 
   def woot
-if true
-if 'woot'
-if 'wang'
-puts 'woot' if true if true
-if "bad things are happening" then
-puts "Oh my god this is a really long line Oh my god this is a really long line Oh my god this is a really long line"
-end
-end
-end
-end
+  if true
+  if 'woot'
+  if 'wang'
+  puts 'woot' if true if true
+  if "bad things are happening" then
+  puts "Oh my god this is a really long line Oh my god this is a really long line Oh my god this is a really long line"
+  end
+  end
+  end
+  end
   end
 
 
