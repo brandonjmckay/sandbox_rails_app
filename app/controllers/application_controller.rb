1b646a8a6006569967cc2bd2890378f9e6b6a462
Brandon J McKay <bmckay@tractionguest.com>
2020-03-13 12:13:32 -0700

    make super nesty method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  def woot
+    if true
+      if 'woot'
+        if 'wang'
+          puts 'woot' if true if true
+        end
+      end
+    end
+  end
 end
